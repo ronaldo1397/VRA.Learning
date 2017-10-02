22e7f928650b0ec6ae508893f82f9bdb67251640
ronaldo1397 <nguyenvanthinhab3@gmail.com>
2017-10-02 09:50:01 +0700

    To study
    Fixed

--- Task_002/Recognition001_Digits.m
+++ Task_002/Recognition001_Digits.m
@@ -3,7 +3,7 @@ function Recognition001_Digits()
     imgTrainAll = loadMNISTImages('./train-images.idx3-ubyte');
     lblTrainAll = loadMNISTLabels('./train-labels.idx1-ubyte');
     fprintf('\nLoad du lieu test');
-    imgTrainAll = loadMNISTImages('./t10k-images.idx3-ubyte');
-    lblTrainAll = loadMNISTLabels('./t10k-labels.idx1-ubyte');
+    imgTestAll = loadMNISTImages('./t10k-images.idx3-ubyte');
+    lblTestAll = loadMNISTLabels('./t10k-labels.idx1-ubyte');
     fprintf('\nKet thuc.');
 end
--- Task_002/Recognition003_Digits.m
+++ Task_002/Recognition003_Digits.m
@@ -2,7 +2,8 @@ function Recognition003_Digits()
     fprintf('\nNap du lieu train');
     imgTrainAll = loadMNISTImages('./train-images.idx3-ubyte');
     lblTrainAll = loadMNISTLabels('./train-labels.idx1-ubyte');
-    fprintf('\nNap du lieu train');
+    fprintf('\nNap du lieu test');
+    
     imgTestAll = loadMNISTImages('./t10k-images.idx3-ubyte');
     lblTestAll = loadMNISTLabels('./t10k-labels.idx1-ubyte');
     
@@ -10,7 +11,7 @@ function Recognition003_Digits()
     
     figure;
     img = imgTrainAll(:, 1);
-    img2D = reshape(img, 28, 28);
+    img2D = reshape(img, 28, 28); 
     strLabelImage = num2str(lblTrainAll(1));
     imshow(img2D);
     title(strLabelImage);
