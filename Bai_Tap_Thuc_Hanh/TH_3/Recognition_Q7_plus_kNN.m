d1a957ce1afcf2e00159a30c591a55972fee6e75
ronaldo1397 <nguyenvanthinhab3@gmail.com>
2017-10-14 19:22:15 +0700

    upDates
    BTTH 3

--- Bai_Tap_Thuc_Hanh/TH_3/Recognition_Q7_plus_kNN.m
+++ Bai_Tap_Thuc_Hanh/TH_3/Recognition_Q7_plus_kNN.m
@@ -1,19 +1,22 @@
-function Recognition_Q7_kNN()
-    imgTrainAll = loadMNISTImages ('./train-images.idx3-ubyte');
+function Recognition_Q7_plus_kNN()
+   imgTrainAll = loadMNISTImages ('./train-images.idx3-ubyte');
     lblTrainAll = loadMNISTLabels ('./train-labels.idx1-ubyte');
     imgTestAll = loadMNISTImages ('./t10k-images.idx3-ubyte');
     lblTestAll = loadMNISTLabels ('./t10k-labels.idx1-ubyte');
     nNumber = size(imgTestAll, 2);
-    rArray = zeros(1, 10);
+    rArray = zeros(11, 11);
+    rArray(1, 1) = -1;
+    for i = 2 : 11
+        rArray(1, i) = i-2;
+        rArray(i, 1) = i-2;
+    end
     Mdl = fitcknn (imgTrainAll', lblTrainAll);
     lblResult = predict(Mdl,imgTestAll');
     nResult = (lblResult == lblTestAll);
     for i = 1 : nNumber
         if(nResult(i) == 0)
-                rArray(lblTestAll(i)+1) =  rArray(lblTestAll(i)+1) + 1;
+            rArray(lblTestAll(i) + 2, lblResult(i) + 2) = rArray(lblTestAll(i) + 2, lblResult(i) + 2) + 1;
         end
     end
-    for i = 1 : 10
-        fprintf('\n So %d sai la: %d ', i - 1, rArray(i));
-    end
+    disp(rArray);
 end
